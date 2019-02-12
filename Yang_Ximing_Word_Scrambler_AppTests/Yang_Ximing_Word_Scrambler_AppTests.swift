5aedbf3c2399751d82a967f9ab28c7750a46023d
Ximing Yang <jamesyang@ximings-mac-mini.local>
2019-02-12 10:29:34 -0500

    Initial Commit

---
+++ Yang_Ximing_Word_Scrambler_AppTests/Yang_Ximing_Word_Scrambler_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Yang_Ximing_Word_Scrambler_AppTests.swift
+//  Yang_Ximing_Word_Scrambler_AppTests
+//
+//  Created by Ximing Yang on 2019-02-12.
+//  Copyright © 2019 Ximing Yang. All rights reserved.
+//
+
+import XCTest
+@testable import Yang_Ximing_Word_Scrambler_App
+
+class Yang_Ximing_Word_Scrambler_AppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
